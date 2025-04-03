e1e6d132151115a212e67b949ff24c2e33a6154d
ARDoagooei <a.r.doagooei@gmail.com>
2025-04-03 12:59:41 +0200

    adding comments

--- src/config/cors-options.js
+++ src/config/cors-options.js
@@ -1,17 +1,24 @@
-// CORS configuration
-const allowedOrigins = [
-    'http://localhost:3000', // Local development server
-    'http://localhost:5173', // Vite frontend server (local)
-    'http://localhost:5174', // Vite frontend server (local)
-];
+// // CORS configuration
+// const allowedOrigins = [
+//     'http://localhost:3000', // Local development server
+//     'http://localhost:5173', // Vite frontend server (local)
+//     'http://localhost:5174', // Vite frontend server (local)
+// ];
+// export const corsOptions = {
+//     origin: (origin, callback) => {
+//         if (allowedOrigins.includes(origin) || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+
+//test mode
 export const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
