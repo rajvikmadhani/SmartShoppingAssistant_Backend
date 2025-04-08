f0b3e7ee09326928893fbac3d861fbc4bb4f18e3
ARDoagooei <a.r.doagooei@gmail.com>
2025-04-08 10:41:36 +0200

    removing auth

--- src/controllers/liveDataController.js
+++ src/controllers/liveDataController.js
@@ -3,11 +3,11 @@ import ScraperService from '../services/scraperService.js';
 
 export const scrapeProduct = asyncHandler(async (req, res, next) => {
     const productQuery = req.query;
-    if (!productQuery.name || !productQuery.brand) {
-        return res
-            .status(400)
-            .json({ error: 'Missing required name and brand attributes. e.g, products?name=iPhone 13&brand=Apple' });
-    }
+    // if (!productQuery.name || !productQuery.brand) {
+    //     return res
+    //         .status(400)
+    //         .json({ error: 'Missing required name and brand attributes. e.g, products?name=iPhone 13&brand=Apple' });
+    // }
 
     const product = await ScraperService.fetchProductData(productQuery);
     res.json(product);
