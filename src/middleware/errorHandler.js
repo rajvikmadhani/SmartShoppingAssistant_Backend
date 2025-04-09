695e7d50bea69777d39804ad38f41824e5306107
ARDoagooei <a.r.doagooei@gmail.com>
2025-04-09 15:21:41 +0200

    UPDATE[project] adding wishlist functionality

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-    process.env.NODE_ENV !== 'production' && console.error(err.stack);
+    console.error(err.stack);
     res.status(err.statusCode || 500).json({
         message: err.message || 'Internal Server Error',
     });
